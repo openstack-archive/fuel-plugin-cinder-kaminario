0255d5b89f5e83867be4c1eccb843b5a42684826
Pradip Rawat <Pradip.Rawat.CTR@kaminario.com>
2016-10-24 14:24:46 +0530

    Update Multipath module
    Change-Id: I05395f5aa9e148aea8a5fc2dccf1212c600193cf
    Co-Authored-By: Chaithanya Kopparthi<chaithanyak@biarca.com>

--- deployment_scripts/puppet/modules/multipath/manifests/init.pp
+++ deployment_scripts/puppet/modules/multipath/manifests/init.pp
@@ -1,9 +1,18 @@
 class multipath {
+$plugin_settings = hiera('cinder_kaminario')
+$multi_0 = $plugin_settings["enable_multipath_0"] 
+$multi_1 = $plugin_settings["enable_multipath_1"]
+$multi_2 = $plugin_settings["enable_multipath_2"]
+$multi_3 = $plugin_settings["enable_multipath_3"]
+$multi_4 = $plugin_settings["enable_multipath_4"]
+$multi_5 = $plugin_settings["enable_multipath_5"]
 
 include ::nova::params
 
-$multipath_packages = [ 'sg3-utils', 'multipath-tools' ]
-package { $multipath_packages: ensure => 'installed' }
+if ( $multi_0 == true) or ($multi_1 == true) or ($multi_2 == true) or ($multi_3 == true) or ($multi_4 == true) or ($multi_5 == true) {
+
+package { sg3-utils: ensure => 'installed' }
+package { multipath-tools: ensure => 'installed' }
 
 nova_config {
   'libvirt/iscsi_use_multipath' :   value => True,
@@ -18,4 +27,5 @@ service { 'nova_compute':
 }
 
 Nova_config<||> ~> Service['nova-compute']
+}
 }
